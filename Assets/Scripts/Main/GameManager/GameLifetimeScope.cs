3f77aa9afd2cbf82e172469f88b86b312e728788
WIN11OHS30671\yoosu <hada.chisaki>
2025-12-31 23:57:22 +0900

    [Chore] メッセージパイプラインとDIコンテナ導入

---
+++ Assets/Scripts/Main/GameManager/GameLifetimeScope.cs
@@ -0,0 +1,23 @@
+using MessagePipe;
+using VContainer;
+using VContainer.Unity;
+using UnityEngine;
+
+public sealed class GameLifetimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        builder.RegisterMessagePipe();
+
+        //オブジェクト作成＆DI登録
+        CreateGameManager(builder);
+    }
+
+    void CreateGameManager(IContainerBuilder builder)
+    {
+        // このLifetimeScopeが付いてるGameObjectの子として生成される
+        builder.RegisterComponentOnNewGameObject<GameManager>(Lifetime.Singleton, "GameManager")
+            .UnderTransform(transform);
+    }
+
+}
